c4e1d8e76f2ed3fa3866c5ac518d7747eebb0f2e
Peter <peterlongnguyen@gmail.com>
2014-11-03 15:22:08 -0800

    fixed ansible sourcing

--- bin/install-ansible.sh
+++ bin/install-ansible.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # install ansible
 git clone git://github.com/ansible/ansible.git /usr/local/ansible --recursive
-source /usr/local/ansible/hacking/env-setup
 cd /usr/local/ansible
 git pull --rebase
 git submodule update --init --recursive
 cd -
-# install cloudwatch dependencies
-ansible-galaxy install -r requirements.yml -p roles
-# execute cloudwatch playbook
-ansible-playbook deploy.yml
--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /usr/local/ansible/hacking/env-setup
 cd /usr/local/ansible-cloudwatch-standalone
 # install cloudwatch dependencies
 ansible-galaxy install -r requirements.yml -p roles
