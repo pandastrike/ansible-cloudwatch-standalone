6665a7bf01f14ef426ef33f21a5165f169f35a49
Peter <peterlnguyen@users.noreply.github.com>
2014-11-07 11:05:20 -0800

    Delete setup.sh (moved to bin/install-ansible.sh)

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# install ansible
-git clone git://github.com/ansible/ansible.git /usr/local/ansible --recursive
-source /usr/local/ansible/hacking/env-setup
-cd /usr/local/ansible
-git pull --rebase
-git submodule update --init --recursive
